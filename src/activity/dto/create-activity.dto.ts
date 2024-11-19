c73e4fc4bbe7c8a4ac10ed1e701e01e317bb392d
ikabeee <carlglz30@gmail.com>
2024-11-19 14:30:12 -0500

    ADD: Create activity dto

--- src/activity/dto/create-activity.dto.ts
+++ src/activity/dto/create-activity.dto.ts
@@ -1,1 +1,24 @@
-export class CreateActivityDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateActivityDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+  @IsString()
+  @IsNotEmpty()
+  start_hour: string;
+  @IsString()
+  @IsNotEmpty()
+  end_hour: string;
+  @IsNumber()
+  @IsNotEmpty()
+  status: number;
+  @IsString()
+  @IsNotEmpty()
+  due_date: string;
+  @IsNumber()
+  taskId: number;
+}
