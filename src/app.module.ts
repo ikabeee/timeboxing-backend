23cd1ad0c1b7fcabb33f3215fdc399b2155e92fa
ikabeee <carlglz30@gmail.com>
2024-11-19 14:28:58 -0500

    Import task, user, activity and prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
 import { UserModule } from './user/user.module';
 import { ActivityModule } from './activity/activity.module';
-import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TaskModule, UserModule, ActivityModule, AuthModule, PrismaModule],
-  controllers: [],
-  providers: [],
+  imports: [TaskModule, UserModule, ActivityModule, PrismaModule],
 })
 export class AppModule {}
