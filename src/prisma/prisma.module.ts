2e3cbc0cc79ee9e2241a9246ab9cda8cd1a6630e
ikabeee <carlglz30@gmail.com>
2024-11-19 14:30:59 -0500

    DELETE: Prisma Service from imports

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [PrismaService],
   providers: [PrismaService],
   exports: [PrismaService],
 })
